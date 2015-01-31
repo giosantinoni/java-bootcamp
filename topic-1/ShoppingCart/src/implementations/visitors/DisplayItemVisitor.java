1a6239fde34f9cf04001613139dc3f2f0a3f3adc
manuel <manuel@gmail.com>
2015-01-31 10:53:34 -0300

    Added offers

--- topic-1/ShoppingCart/src/implementations/visitors/DisplayItemVisitor.java
+++ topic-1/ShoppingCart/src/implementations/visitors/DisplayItemVisitor.java
@@ -4,19 +4,18 @@ import implementations.products.Computer;
 import implementations.products.Tablet;
 import interfaces.IVisitor;
 
-public class DisplayItemVisitor implements IVisitor{
+public class DisplayItemVisitor implements IVisitor {
 
 	@Override
 	public void visitComputer(Computer computer) {
 		System.out.println(computer.getName() + "....... $" + computer.getPrice());
-		
+
 	}
 
 	@Override
 	public void visitTablet(Tablet tablet) {
 		System.out.println(tablet.getName() + "....... $" + tablet.getPrice());
-		
+
 	}
 
-	
 }
